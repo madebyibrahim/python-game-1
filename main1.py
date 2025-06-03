77f34e4298aabeb00d79b3f9033c78acb04b67eb
Ibrahim <brahimnakad@gmail.com>
2025-06-03 17:24:35 +0300

    working game v1

--- main1.py
+++ main1.py
@@ -1,101 +1,140 @@
+## when we finish change the theme and make it a mouth thats eats food and avoids poison
+
 import pygame
 import time
 import random
+import sys
+import os
+
+def resource_path(relative_path):
+    """ Get absolute path to resource, works for dev and for PyInstaller """
+    try:
+        # PyInstaller creates a temp folder and stores path in _MEIPASS
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = os.path.abspath(".")
+
+    return os.path.join(base_path, relative_path)
+
+
 
 pygame.init()
 
 displayWidth = 1000
 displayHeight = 680
-ballWidth = 250 #adjust as fit
+ballWidth = 250  # adjust as fit
 
-blackC = (0,0,0)
-whiteC = (255,255,255)
-redC = (255,0,0)
-greenC = (0,255,0)
-blueC = (0,0,255)
-blockC = (169,43,97)
-lightGreenC = (116,245,24)
-lightRedC = (224,52,72)
+blackC = (0, 0, 0)
+whiteC = (255, 255, 255)
+redC = (255, 0, 0)
+greenC = (0, 255, 0)
+blueC = (0, 0, 255)
+blockC = (169, 43, 97)
+lightGreenC = (0, 204, 0)
+lightRedC = (204, 0, 0)
 
 clock = pygame.time.Clock()
 
-ballImg = pygame.image.load("250x250 img 1.png")
+ballImg = pygame.image.load(resource_path("250x250_img_1.png"))
 
 gameDisplay = pygame.display.set_mode((displayWidth, displayHeight))
 pygame.display.set_caption("The Hungry Mouth")
 
+
+
 def thingsDodged(count):
     font = pygame.font.SysFont(None, 25)
-    text = font.render("Dodged: " + str(count), True , blackC)
-    gameDisplay.blit(text, (0,0))
+    text = font.render("Dodged: " + str(count), True, blackC)
+    gameDisplay.blit(text, (0, 0))
+
 
 def things(thingX, thingY, thingW, thingH, color):
     pygame.draw.rect(gameDisplay, color, [thingX, thingY, thingW, thingH])
 
-def ballFunc(x,y):
-    gameDisplay.blit(ballImg, (x,y))
+
+def ballFunc(x, y):
+    gameDisplay.blit(ballImg, (x, y))
+
 
 def textObjects(text, font):
     textSurface = font.render(text, True, blackC)
     return textSurface, textSurface.get_rect()
 
+
 def messageDisplay(text):
-    largeText = pygame.font.Font('freesansbold.ttf', 115)
+    largeText = pygame.font.Font("freesansbold.ttf", 115)
     textSurf, textRect = textObjects(text, largeText)
-    textRect.center =  ((displayWidth/2), (displayHeight/2))
+    textRect.center = (displayWidth / 2, displayHeight / 2)
     gameDisplay.blit(textSurf, textRect)
     pygame.display.update()
     time.sleep(2)
     gameLoop()
 
+
 def crash():
-    messageDisplay('You crashed!')
+    messageDisplay("You crashed!")
+
 
 def gameIntro():
     intro = True
     while intro:
         for event in pygame.event.get():
-            print(event)
-            if event.type ==  pygame.QUIT:
+            if event.type == pygame.QUIT:
                 pygame.quit()
                 quit()
-        gameDisplay.fill(whiteC)
-        largeText = pygame.font.Font("freesansbold.ttf", 80)
-        textSurf,textRect = textObjects("The Hungry Mouth", largeText)
-        textRect.center = ((displayWidth/2), (displayHeight/4))
-        gameDisplay.blit(textSurf, textRect)
-        # pygame.draw.line(gameDisplay, (0, 0, 255), (displayWidth/2, 0), (displayWidth/2, displayHeight)) # vertical center line
 
-        mousePos = pygame.mouse.get_pos()
-        if (displayWidth/2-180)+120>mousePos[0]>(displayWidth/2-180) and 450+60>mousePos[1]>450:
-            pygame.draw.rect(gameDisplay, lightGreenC, ((displayWidth/2-180),450,120,60))
-        else:
-            pygame.draw.rect(gameDisplay, greenC,((displayWidth/2-180),450,120,60))
-        if (displayWidth/2+60) + 120> mousePos[0] > (displayWidth/2+60) and 450+ 60 >mousePos[1] > 450:
-            pygame.draw.rect(gameDisplay, lightRedC, ((displayWidth/2+60),450,120,60))
-        else: 
-            pygame.draw.rect(gameDisplay, redC, ((displayWidth/2+60),450,120,60))
+        gameDisplay.fill(whiteC)
+
+        largeText = pygame.font.Font("freesansbold.ttf", 80)
+        textSurf, textRect = textObjects("The Hungry Mouth", largeText)
+        textRect.center = (displayWidth / 2, displayHeight / 4)
+        gameDisplay.blit(textSurf, textRect)
+
+        button("PLAY", displayWidth / 2 - 180, 450, 120, 60, greenC, lightGreenC, gameLoop)
+        button("QUIT", displayWidth / 2 + 60, 450, 120, 60, redC, lightRedC, quitGame)
 
         pygame.display.update()
         clock.tick(30)
 
+
+def quitGame():
+    pygame.quit()
+    quit()
+
+
+def button(msg, x, y, w, h, ic, ac, action=None):
+    mousePos = pygame.mouse.get_pos()
+    mouseClicked = pygame.mouse.get_pressed()
+    # print(mouseClicked)  # can be commented out once you confirm clicks work
+
+    if x < mousePos[0] < x + w and y < mousePos[1] < y + h:
+        pygame.draw.rect(gameDisplay, ac, (x, y, w, h))
+        if mouseClicked[0] == 1 and action is not None:
+            action()
+    else:
+        pygame.draw.rect(gameDisplay, ic, (x, y, w, h))
+
+    smallText = pygame.font.Font("freesansbold.ttf", 30)
+    textSurf, textRect = textObjects(msg, smallText)
+    textRect.center = (x + (w / 2), y + (h / 2))
+    gameDisplay.blit(textSurf, textRect)
+
+
 def gameLoop():
     x = 250
     y = 354
-    # x = (displayWidth * 0.45) # random x coordinate 
-    # y = (displayHeight * 0.8) # x is positive from left to right and y is postive from up to down.
     xChange = 0
+
     thingWidth = 100
     thingHeight = 100
-    thingStartX = random.randrange(0,displayWidth-thingWidth)
-    # thingStartX = random.randrange(0,displayWidth) # this is wrong
-    thingStartY = -3600
-    thingSpeed = 7 # can increase or decrease to affect difficulty
-    thingCount = 1
+    thingStartX = random.randrange(0, displayWidth - thingWidth)
+    thingStartY = -1500
+    thingSpeed = 7  # can increase or decrease to affect difficulty
     dodged = 0
+
     gameExit = False
 
-    while not gameExit: 
+    while not gameExit:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
@@ -106,35 +145,41 @@ def gameLoop():
                 if event.key == pygame.K_RIGHT:
                     xChange = 15
             if event.type == pygame.KEYUP:
-                # if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:   # not really needed
-                    xChange = 0
-        x += xChange   
+                xChange = 0  # stop moving when key is released
+
+        x += xChange
+
         gameDisplay.fill(whiteC)
         things(thingStartX, thingStartY, thingWidth, thingHeight, blockC)
+
         thingStartY += thingSpeed
-        ballFunc(x,y)        
+        ballFunc(x, y)
         thingsDodged(dodged)
+
+        # Boundary check: crash if mouth goes off-screen
         if (x > (displayWidth - ballWidth)) or (x < 0):
             crash()
+
+        # When the “thing” moves past bottom, reset and speed up
         if thingStartY > displayHeight:
             thingStartY = 0 - thingHeight
-            thingStartX = random.randrange(0,displayWidth-thingWidth)
-            # thingStartX = random.randrange(0,displayWidth)
-            dodged+=1
-            thingSpeed+=1
-            thingWidth+=(dodged*1.2)
-            # thingWidth+=(dodged*1.2)
+            thingStartX = random.randrange(0, displayWidth - thingWidth)
+            dodged += 1
+            thingSpeed += 1
+            thingWidth += (dodged)
+
+        # Collision: check vertical overlap first, then horizontal
         if y < (thingStartY + thingHeight):
-            # print('y crossover')   # it works, just commented out because its a bit spammy
-            # <--- Added parentheses here to group the two “collision” checks clearly:
-            if ((x > thingStartX and x < (thingStartX + thingWidth)) or ((x + ballWidth) > thingStartX and (x + ballWidth) < (thingStartX + thingWidth))):
-                # print('x crossover')  # it works, just commented out because its a bit spammy
+            if (
+                (x > thingStartX and x < (thingStartX + thingWidth))
+                or ((x + ballWidth) > thingStartX and (x + ballWidth) < (thingStartX + thingWidth))
+            ):
                 crash()
 
         pygame.display.update()
         clock.tick(60)
 
+
 gameIntro()
-gameLoop()
-pygame.quit()
-quit()
+
+
